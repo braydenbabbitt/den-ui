c2b120a8cd17ee3c7bc35d951be92a801980ad95
Brayden Babbitt <brayden@babbitts.net>
2022-10-20 16:43:10 -0600

    fix(hooks): fixed serialization adding extra quotes

--- src/hooks/utils/serialization.ts
+++ src/hooks/utils/serialization.ts
@@ -1,9 +1,12 @@
 export const serializeValue = <T>(value: T) => {
-  try {
-    return JSON.stringify(value);
-  } catch (error) {
-    throw new Error('Failed to serialize value used in useLocalStorage');
+  if (typeof value !== 'string') {
+    try {
+      return JSON.stringify(value);
+    } catch (error) {
+      throw new Error('Failed to serialize value used in useLocalStorage');
+    }
   }
+  return value;
 };
 
 export const deserializeValue = (value: string) => {
