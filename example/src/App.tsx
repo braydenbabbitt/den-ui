57942baa017c85b65ff98ba2f896b164984c4017
Brayden Babbitt <brayden@babbitts.net>
2022-10-20 16:44:33 -0600

    chore(example): various cleanup and changes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { useDestructableLocalStorage } from 'den-ui';
+import { Button, useDestructableLocalStorage } from 'den-ui';
 import './App.css';
 
 function App() {
   const [testStorage, setTestStorage] = useDestructableLocalStorage<string | undefined>('testKey', undefined);
+  const localStorageValue = localStorage.getItem('testKey') ?? '';
+
+  console.info([testStorage, localStorageValue]);
 
   return (
     <main>
       <h1>Test</h1>
-      <button
+      <Button
         onClick={() => {
           setTestStorage(testStorage ? undefined : 'test');
         }}
-      ></button>
+      >
+        Write Local Storage
+      </Button>
+      <h2>State value: {testStorage}</h2>
+      <h2>Local storage value: {localStorageValue}</h2>
     </main>
   );
 }
