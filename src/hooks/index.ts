c70d7c3d087767195d281724f4b735bcb7825da6
Brayden Babbitt <brayden@babbitts.net>
2022-11-04 22:09:47 -0600

    fix(hooks): refactored use-destructible-local-storage

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,2 @@
 export { useBrowserColorScheme } from './use-browser-color-scheme/use-browser-color-scheme.hook';
-export { useDestructableLocalStorage } from './use-destructable-local-storage/use-destructable-local-storage.hook';
+export { useDestructibleLocalStorage as useDestructableLocalStorage } from './use-destructible-local-storage/use-destructible-local-storage.hook';
