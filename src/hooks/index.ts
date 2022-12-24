069ce02d027e268b778f813be40cd6da7a6580ce
Brayden Babbitt <brayden@babbitts.net>
2022-12-24 11:09:42 -0700

    fix(hooks): added export for typedJSONEncoding

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { useBrowserColorScheme } from './use-browser-color-scheme/use-browser-color-scheme.hook';
 export { useDestructibleLocalStorage } from './use-destructible-local-storage/use-destructible-local-storage.hook';
+export { useTypedJSONEncoding } from './use-typed-json-encoding/use-typed-json-encoding.hook';
